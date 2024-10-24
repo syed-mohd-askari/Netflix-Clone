93d157b5af584bc085fe7926c4398e24326569af
Syed Mohd Askari <mohdaskari48@gmail.com>
2024-10-24 14:04:14 +0530

    updated UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -69,8 +69,8 @@ const Login = () => {
     <div className='relative font-sans-serif h-screen bg-[url("https://assets.nflxext.com/ffe/siteui/vlv3/f272782d-cf96-4988-a675-6db2afd165e0/web/IN-en-20241008-TRIFECTA-perspective_b28b640f-cee0-426b-ac3a-7c000d3b41b7_small.jpg")] bg-cover'>
       <div className="absolute inset-0 bg-black opacity-75"></div>
       <div className="relative z-10 h-10px w-full"><Header/></div>
-      <div className="h-screen flex items-center justify-center">
-        <div className="relative p-20 w-[450px] flex flex-col gap-10 justify-center z-10 text-[white] bg-black bg-opacity-55 rounded-lg">
+      <div className="relative h-screen flex items-center justify-center ">
+        <div className="absolute top-40 py-20 px-14  w-[450px] flex flex-col gap-10 justify-center z-10 text-[white] bg-black bg-opacity-55 rounded-lg">
           <p className='text-7xl font-[900] text-[2rem]'>{isSigninForm ? 'Sign In' : 'Sign Up'}</p>
           <form className='flex flex-col gap-5' onSubmit={(e)=>e.preventDefault()}>
             {!isSigninForm && <input className='p-4 rounded-md bg-white bg-opacity-5 border' type="text" placeholder='Full Name' />}
